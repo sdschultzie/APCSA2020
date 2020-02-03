458f5414bbf2f456c8c26b2436d6a4731f331321
schultzc8791 <schultzc8791@CA-SU-F106-10.sduhsd.lan>
2020-02-03 10:40:56 -0800

    School to Git 2/3/20 10:40am

--- APCS-A-2020/src/Unit0/Variables.java
+++ APCS-A-2020/src/Unit0/Variables.java
@@ -39,7 +39,7 @@ public class Variables
 		System.out.println("*                               *");
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
-		System.out.println("*8 bit - byteOne = "+byteOne+"\t*");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
 		System.out.println("*16 bit - shortOne = "+shortOne+"\t*");
 		System.out.println("*32 bit - intOne = "+intOne+"\t*");
 		System.out.println("*64 bit - longOne = "+longOne+"\t*");
