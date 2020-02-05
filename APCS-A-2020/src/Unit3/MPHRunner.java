83599422a7cf5d25edd9cbe4ca5228336a1ded53
Chris <Chris@DESKTOP-DVNVJDJ>
2020-02-05 14:25:32 -0800

    Home to Git 2/5/20

--- APCS-A-2020/src/Unit3/MPHRunner.java
+++ APCS-A-2020/src/Unit3/MPHRunner.java
@@ -16,22 +16,21 @@ public class MPHRunner
 	public static void main( String[] args )
 	{
 		Scanner keyboard = new Scanner(in);
-
-		out.print("Enter the distance :: ");
-		int dist = keyboard.nextInt();
-
-		out.print("Enter the hours :: ");
-		int hrs = keyboard.nextInt();
-
-		out.print("Enter the minutes :: ");
-		int mins = keyboard.nextInt();
-
-		MilesPerHour test = new MilesPerHour(dist, hrs, mins);
-		test.calcMPH();
-		test.print();
 		
-		//add more test cases
-		
-		
+		for (int i=0; i<4; i++) {
+				
+			out.print("Enter the distance :: ");
+			int dist = keyboard.nextInt();
+	
+			out.print("Enter the hours :: ");
+			int hrs = keyboard.nextInt();
+	
+			out.print("Enter the minutes :: ");
+			int mins = keyboard.nextInt();
+	
+			MilesPerHour test = new MilesPerHour(dist, hrs, mins);
+			test.calcMPH();
+			test.print();	
+		}
 	}
 }
--- APCS-A-2020/src/Unit3/MilesPerHour.java
+++ APCS-A-2020/src/Unit3/MilesPerHour.java
@@ -24,18 +24,29 @@ public class MilesPerHour
 
 	public MilesPerHour(int dist, int hrs, int mins)
 	{
+		setNums(dist, hrs, mins);
+		mph=0.0;
 	}
 
 	public void setNums(int dist, int hrs, int mins)
 	{
+		distance = dist;
+		hours = hrs;
+		minutes = mins;
 	}
 
 	public void calcMPH()
 	{
+		double totalTime = hours + (double) minutes/60;
+		mph = distance/totalTime;
 	}
 
 	public void print()
 	{
+		String mphString = String.format("%.0f", mph);
+		System.out.println("\n\n");
+		System.out.println(distance + " miles in " + hours + " hours and " + minutes + " minutes = " + mphString + " MPH.");
+		System.out.println("\n\n");
 	}
 	
 	//create a print or toString or both
