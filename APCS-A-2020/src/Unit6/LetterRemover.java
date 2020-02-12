d363e93a985ab33fb215513c59e872dd321190ec
schultzc8791 <schultzc8791@CA-SU-F106-11.sduhsd.lan>
2020-02-12 11:56:36 -0800

    School to Git 2/12/20

---
+++ APCS-A-2020/src/Unit6/LetterRemover.java
@@ -0,0 +1,40 @@
+package Unit6;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*;
+
+public class LetterRemover
+{
+    private String sentence;
+    private char lookFor;
+
+	public LetterRemover()
+	{
+		setRemover("hello chris", "c");
+	}
+
+	public LetterRemover(String s, char c)
+	{
+		setRemover(s, c);
+	}
+	
+	
+	public void setRemover(String s, char rem)
+	{
+		sentence = s;
+		lookFor = rem;
+	}
+
+	public String removeLetters()
+	{
+		String cleaned=sentence;
+		return cleaned;
+	}
+
+	public String toString()
+	{
+		return sentence + " - letter to remove " + lookFor;
+	}
+}
