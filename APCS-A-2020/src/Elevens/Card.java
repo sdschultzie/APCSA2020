2dbc258e85eaa05d218e6e3151d625c79ae185ab
schultzc8791 <schultzc8791@CA-SU-F106-11.sduhsd.lan>
2020-02-24 10:55:02 -0800

    School to Git 2/24/20

---
+++ APCS-A-2020/src/Elevens/Card.java
@@ -0,0 +1,28 @@
+package Elevens;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public class Card
+{
+	public static final String FACES[] = {"ZERO","ACE","TWO","THREE","FOUR",
+			"FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
+
+	//instance variables
+		//String suit
+		//int face
+
+  	//constructors
+
+
+	// modifiers
+		//set methods
+
+
+  	//accessors
+		//get methods
+
+
+  	//toString
+
+ }
