081f59d6ff5094072c36b15eb5c932b61bace1ed
User <User@DESKTOP-53B49VU>
2020-05-06 15:52:21 -0700

    Home to Git 5/6/20

--- APCS-A-2020/src/Unit05/FirstLastVowel.java
+++ APCS-A-2020/src/Unit05/FirstLastVowel.java
@@ -10,6 +10,8 @@ public class FirstLastVowel
    public static String go( String a )
 	{
 	   String vowels = "aeiouAEIOU";
+	   char c = 'o';
+	   System.out.println(c == vowels.charAt(3));
 	   String firstChar =  a.substring(0,1);
 	   String lastChar = a.substring(a.length()-1);
 	   if (vowels.contains(firstChar) || vowels.contains(lastChar)) {
